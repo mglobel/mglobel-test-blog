4c329ca7f07b2b69e5192da14e544ea75a4a3c61
Michael Lobel <mglobel@gmail.com>
2016-03-29 18:45:56 -0500

    added bookshelf algolia components

--- app/components/Bookshelf.jsx
+++ app/components/Bookshelf.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import instantsearch from 'instantsearch.js';
 
 class Bookshelf extends React.Component {
+  constructor(props) {
+    super(props);
+    this.search = instantsearch({
+      appId: '5WULZ0J5FB',
+      apiKey: 'e4a3465721383a39b91a2d391a37c68a',
+      indexName: 'bookshelf',
+    });
+  }
+
+  componentDidMount() {
+    this.search.addWidget(
+      instantsearch.widgets.searchBox({
+        container: '#search-box',
+        placeholder: 'Search for books on the shelf...'
+      })
+    );
+    
+    this.search.addWidget(
+      instantsearch.widgets.hits({
+        container: '#hits-container',
+        hitsPerPage: 10,
+        templates: {
+          empty: 'No results',
+          item: '<a href="/#/book/{{permalink}}">{{title}}</a><br /> by {{{author}}}',
+        },
+      })
+    );
+    this.search.start();
+  }
+
   render() {
-    return (
+    return(
       <div>
-        It's a bookshelf.
+        <div id="search-box" style={{display: 'none'}}></div>
+        <div id="hits-container"></div>
       </div>
     );
   }
